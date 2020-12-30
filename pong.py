4fa8307c598d0548a5ffb53163b4988eb51cd29b
Brian Shin <waterdrinker26@gmail.com>
2020-12-29 20:20:15 -0700

    Create display window

--- pong.py
+++ pong.py
@@ -2,3 +2,19 @@ import time
 
 import pygame
 
+pygame.init()
+
+DISPLAY_WIDTH = 950
+DISPLAY_HEIGHT = 570
+
+FPS = 60
+
+CLOCK = pygame.time.Clock()
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+GAME_DISPLAY = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+
