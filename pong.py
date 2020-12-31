8f1f3a1caf19608acc453f49d3ad52f0130035d8
Brian Shin <waterdrinker26@gmail.com>
2020-12-30 20:27:04 -0700

    Create player class init function

--- pong.py
+++ pong.py
@@ -17,6 +17,20 @@ WHITE = (255, 255, 255)
 
 GAME_DISPLAY = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+    """player class for player object"""
+    def __init__(self, x_position):
+        self.width = 15
+        self.height = 90
+
+        self.color = WHITE
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x_position
+        self.rect.y = (DISPLAY_HEIGHT // 2) - self.height // 2
+
 def game_loop():
     """execute game loop and game logic"""
     game_exit = False
